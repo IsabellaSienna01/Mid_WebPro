32c43da96ec50465f5d1cf51a036959654cfdaa5
Ctrl-Zack <zakyzein2006@gmail.com>
2025-10-21 05:57:39 +0700

    fix: include app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Library' }}</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="pt-16">
     @include('layouts.navbar')
