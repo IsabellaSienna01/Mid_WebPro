ab3ddae5710afa2ce04c7aab1e42600ac570a11a
Ctrl-Zack <zakyzein2006@gmail.com>
2025-10-20 09:42:40 +0700

    feat: layout sementara

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Library' }}</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="">
+    @include('layouts.navbar')
+
+    <main class="container mx-auto px-6 py-10">
+        @yield('content')
+    </main>
+
+    @include('layouts.footer')
+</body>
+</html>
