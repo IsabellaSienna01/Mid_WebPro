ab8915007b5db06b36e91ff6ad5bbd0d3f564882
Ctrl-Zack <zakyzein2006@gmail.com>
2025-10-21 11:27:12 +0700

    fix: fix login admin

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -5,15 +5,21 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
 {
-    public function handle(Request $request, Closure $next, $role): Response
+    public function handle(Request $request, Closure $next, ...$roles)
     {
-        if (!Auth::check() || Auth::user()->role !== $role) {
-            abort(403, 'Unauthorized');
+        if (!Auth::check()) {
+            return redirect()->route('login');
         }
+
+        $userRole = Auth::user()->role;
+
+        if (!in_array($userRole, $roles)) {
+            abort(403, 'Unauthorized access.');
+        }
+
         return $next($request);
     }
 }
